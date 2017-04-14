c7fe64d1c0afe0e16bc449d7bdf92e7af7c0f150
Cesar Gimenes <crg@crg.eti.br>
2017-04-13 22:27:50 -0300

    Prim's Algorithm implemented

--- mst/main.go
+++ mst/main.go
@@ -7,21 +7,146 @@ import (
 	"github.com/crgimenes/graphos/coreGame"
 )
 
+type dot struct {
+	X int
+	Y int
+}
+
 var (
-	cs *coreScreen.Instance
+	cg *coreGame.Instance
+
+	xAux, yAux int
+
+	dotMain      []dot
+	dotUnreached []dot
+	dotReached   []dot
 )
 
-func update(screen *coreScreen.Instance) error {
+var color byte
+
+func update(screen *coreGame.Instance) error {
+
+	screen.CurrentColor = 0
+
+	screen.Clear()
+
+	screen.CurrentColor = 0xf
+	color++
+	if color > 15 {
+		color = 0
+	}
+
+	for i := 0; i < len(dotMain); i++ {
+		x := random(1, 5)
+		switch x {
+		case 1:
+			dotMain[i].X++
+		case 2:
+			dotMain[i].X--
+		case 3:
+			dotMain[i].Y++
+		case 4:
+			dotMain[i].Y--
+
+		}
+	}
+
+	//screen.CurrentColor = 0xf
+
+	for i := 0; i < len(dotMain); i++ {
+		screen.DrawFilledCircle(dotMain[i].X, dotMain[i].Y, 4)
+
+		/*
+			if xAux == 0 {
+				xAux = dotMain[i].X
+				yAux = dotMain[i].Y
+				screen.DrawFilledCircle(dotMain[i].X, dotMain[i].Y, 4)
+			} else {
+				screen.DrawFilledCircle(dotMain[i].X, dotMain[i].Y, 4)
+				screen.DrawLine(xAux, yAux, dotMain[i].X, dotMain[i].Y)
+				xAux = dotMain[i].X
+				yAux = dotMain[i].Y
+			}
+		*/
+
+	}
+
+	// --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+	//screen.CurrentColor = 0xf
+
+	for i := 0; i < len(dotMain); i++ {
+		if i == 0 {
+			dotReached = append(dotReached, dotMain[0])
+		} else {
+			dotUnreached = append(dotUnreached, dotMain[i])
+		}
+	}
+
+	var rIndex int
+	var uIndex int
+
+	for len(dotUnreached) > 0 {
+		var record = 1000000
+		for r := 0; r < len(dotReached); r++ {
+			for u := 0; u < len(dotUnreached); u++ {
+				r1 := dotReached[r]
+				u1 := dotUnreached[u]
+
+				d := coreGame.Distance(r1.X, r1.Y, u1.X, u1.Y)
+				if d < record {
+					record = d
+					uIndex = u
+					rIndex = r
+				}
+			}
+		}
+
+		/*
+			screen.DrawFilledCircle(
+				dotReached[rIndex].X,
+				dotReached[rIndex].Y, 4)
+		*/
+		screen.DrawLine(
+			dotReached[rIndex].X,
+			dotReached[rIndex].Y,
+			dotUnreached[uIndex].X,
+			dotUnreached[uIndex].Y)
+
+		dotReached = append(dotReached, dotUnreached[uIndex])
+		dotUnreached = RemoveDot(dotUnreached, uIndex)
+
+	}
+	dotReached = nil
 	return nil
+}
+
+func RemoveDot(s []dot, index int) []dot {
+	return append(s[:index], s[index+1:]...)
+}
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	cs = coreGame.Get()
-	cs.ScreenHandler = update
-	cs.Title = "Minimum Spanning Tree (Prim's Algorithm)"
-	cs.CurrentColor = 0x9
+	cg = coreGame.Get()
 
-	cs.Run()
+	for i := 0; i < 60; i++ {
+
+		d := dot{
+			X: random(10, cg.Width-10),
+			Y: random(10, cg.Height-10),
+		}
+		dotMain = append(dotMain, d)
+	}
+
+	cg.Scale = 2
+	cg.ScreenHandler = update
+	cg.Title = "Minimum Spanning Tree - Prim's Algorithm"
+	cg.CurrentColor = 0x0
+
+	cg.Run()
 }
